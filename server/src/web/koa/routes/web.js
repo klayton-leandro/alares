081d5041facaa2b1cd1feba5a9f89589a27b3c44
Klayton leandro <49959858+klayton-leandro@users.noreply.github.com>
2023-04-22 01:48:51 -0300

    migrando function

--- server/src/web/koa/routes/web.js
+++ server/src/web/koa/routes/web.js
@@ -1,7 +1,7 @@
 import Router from "@koa/router";
 
 import { checkHealth } from "../controllers/helth-check-controller.js";
-import { storeCurrentUserAction, AuthCurrentUserAction } from "../controllers/user-controller.js";
+import { AuthCurrentUserAction } from "../controllers/user-controller.js";
 import errorHandlingMiddleware from "../middlewares/error-handling-middleware.js";
 import knexTransactionMiddleware from "../middlewares/knex-transaction-middleware.js";
 
