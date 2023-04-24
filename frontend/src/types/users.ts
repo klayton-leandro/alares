451e33692da9fd7d582612bc46a72c29e7321ae1
Klayton leandro <49959858+klayton-leandro@users.noreply.github.com>
2023-04-24 10:05:07 -0300

    Update users.ts

--- frontend/src/types/users.ts
+++ frontend/src/types/users.ts
@@ -3,19 +3,6 @@ export type User = {
     name: string
     email: string
     phone: string
-    birthDate: string
-    cpf: string
-    experience: boolean
-    status?: string
-    statusId?: number
     role?: string
     roleId?: number
-    address : {
-        cityId: number
-        number: string
-        publicArea: string
-        neighbourhood: string
-        additionalInformation: string
-        postalCode: string
-    }
 }
